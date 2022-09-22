3c6c5b82872ebd59e6cedf03246cb1a890898efb
Nawel Grine <nawelgrineb@gmail.com>
2022-09-22 16:21:23 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,36 @@
+let header = document.querySelector("header");
+let searchCard = document.querySelector(".searchcard");
+let input = document.querySelector("input");
+let sectionDescription=document.querySelector(".description")
+// let url = "http://gateway.marvel.com/v1/comics/?ts=1&apikey=";
+// let key =
+
+//     fetch(url + key)
+//         .then((response) => {
+//             return response.json();
+//         });
+//         .then((data) => {
+            
+//         });
+
+
+// ------ animations js
+window.addEventListener('scroll', (event) => {
+
+    header.style.height = "40vh";
+    searchCard.style.visibility = "visible";
+    sectionDescription.style.height = "60vh";
+
+});
+
+
+
+//------ API CALL
+
+
+
+
+
+
+
+
